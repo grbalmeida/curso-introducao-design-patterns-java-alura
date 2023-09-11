d36cfd5f5783a6f31f9b5635a2804f0a200898be
grbalmeida <g.r.almeida@live.com>
2023-09-11 15:25:17 -0300

    Calculando descontos

---
+++ loja/src/br/com/alura/loja/TestesDescontos.java
@@ -0,0 +1,21 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.desconto.CalculadoraDeDescontos;
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class TestesDescontos {
+
+	public static void main(String[] args) {
+		Orcamento orcamento = new Orcamento(new BigDecimal("200"), 6);
+		Orcamento segundoOrcamento = new Orcamento(new BigDecimal("200"), 4);
+		Orcamento terceiroOrcamento = new Orcamento(new BigDecimal("1000"), 1);
+		
+		CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
+		System.out.println(calculadora.calcular(orcamento)); // 20.0
+		System.out.println(calculadora.calcular(segundoOrcamento)); // 0
+		System.out.println(calculadora.calcular(terceiroOrcamento)); // 100.0
+	}
+
+}
