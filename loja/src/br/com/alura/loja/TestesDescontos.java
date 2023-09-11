fa892e3cb14fcaeeb0ef6e245f2d7d5e152c2e70
grbalmeida <g.r.almeida@live.com>
2023-09-11 15:45:51 -0300

    Aplicando o pattern Chain of Responsibility

--- loja/src/br/com/alura/loja/TestesDescontos.java
+++ loja/src/br/com/alura/loja/TestesDescontos.java
@@ -11,11 +11,13 @@ public class TestesDescontos {
 		Orcamento orcamento = new Orcamento(new BigDecimal("200"), 6);
 		Orcamento segundoOrcamento = new Orcamento(new BigDecimal("200"), 4);
 		Orcamento terceiroOrcamento = new Orcamento(new BigDecimal("1000"), 1);
+		Orcamento quartoOrcamento = new Orcamento(new BigDecimal("1000"), 6);
 		
 		CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
 		System.out.println(calculadora.calcular(orcamento)); // 20.0
 		System.out.println(calculadora.calcular(segundoOrcamento)); // 0
-		System.out.println(calculadora.calcular(terceiroOrcamento)); // 100.0
+		System.out.println(calculadora.calcular(terceiroOrcamento)); // 50.0
+		System.out.println(calculadora.calcular(quartoOrcamento)); // 150.0
 	}
 
 }
